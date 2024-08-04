26e486b1cd9991cb2c9cf12d6dca85542de56232
SoorajTharumia <sendsooraj@gmail.com>
2024-08-04 15:35:11 -0400

    updated contact component

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -19,7 +19,7 @@ const contact = () => {
           </div>
           <div className="grid gap-2">
             <label htmlFor="message" className="font-medium ">Message</label>
-            <textarea id="message" name="message" placeholder="What can we help you with?" className="min-h-[150px] w-full rounded-md shadow-lg text-slate-600 bg-indigo-200 border border-gray-300 p-4 focus:outline-none focus:ring focus:ring-blue-200"></textarea>
+            <textarea id="message" name="message" placeholder="Enter your message" className="min-h-[150px] w-full rounded-md shadow-lg text-slate-600 bg-indigo-200 border border-gray-300 p-4 focus:outline-none focus:ring focus:ring-blue-200"></textarea>
           </div>
           <div className="flex justify-center mt-4">
             <button type="submit" className="text-slate-600 my-5 mx-3 hover:bg-indigo-400 bg-indigo-200 px-5 py-2 rounded-full">
