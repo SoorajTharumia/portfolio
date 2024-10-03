0c303d35ca21da7c233a63fa68c5e98c32bb5e84
SoorajTharumia <sendsooraj@gmail.com>
2024-10-03 10:09:51 -0400

    analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Analytics } from "@vercel/analytics/react"
+import { clarity } from 'react-microsoft-clarity';
 import Home from './components/Home/home'
 import About from './components/About/about'
 import Project from './components/Projects/projects'
@@ -8,6 +9,7 @@ import Navbar from './components/Navbar/navbar'
 import Footer from './components/Footer/footer'
 
 function App() {
+  clarity.init("ocvtuw4ab5")
   return (
     <div className="App" class="bg-slate-100">
       <header className="App-header ">
