0e5c7fcaa4a84cf460e4c11f008a17f7a7ba2db4
SoorajTharumia <sendsooraj@gmail.com>
2025-03-05 00:25:11 -0500

    footer change

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope, FaRocket } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="bg-gray w-full h-20 flex justify-center items-center">
-      <div className="flex items-center">
+    <div className="bg-gray w-full h-24 flex flex-col justify-center items-center">
+      <div className="flex items-center mb-2">
         <span className="text-2xl font-bold mr-4">Sooraj Tharumia</span>
         <a href="https://www.linkedin.com/in/soorajtharumia/" target="_blank" rel="noopener noreferrer" className="mr-3">
           <FaLinkedin className="text-2xl hover:text-blue-500" />
@@ -15,6 +15,9 @@ const Footer = () => {
         <a target="_blank" rel="noopener noreferrer" href="mailto:sendsooraj@gmail.com">
           <FaEnvelope className="text-2xl hover:text-blue-500" />
         </a>
+      </div>
+      <div className="text-lg flex items-center">
+        Crafted with React and Tailwind CSS <FaRocket className="text-2xl ml-2 hover:text-blue-500" />
       </div>
     </div>
   );
